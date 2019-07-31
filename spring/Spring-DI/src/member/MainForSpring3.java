864dde2d4c110f707c040a14959d595731438a18
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-07-31 12:27:21 +0900

    12:27

--- spring/Spring-DI/src/member/MainForSpring3.java
+++ spring/Spring-DI/src/member/MainForSpring3.java
@@ -2,6 +2,8 @@ package member;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import config.AppContext;
+
 public class MainForSpring3 {
 
 	public static void main(String[] args) {
@@ -9,10 +11,18 @@ public class MainForSpring3 {
 		AnnotationConfigApplicationContext ctx = 
 				new AnnotationConfigApplicationContext(AppContext.class);
 		
-		MemberDao dao = ctx.getBean("memberDao", MemberDao.class);
+		MemberDao dao1 = ctx.getBean("memberDao", MemberDao.class);
+		MemberDao dao2 = ctx.getBean("memberDao", MemberDao.class);
+		
+		System.out.println("dao1==dao2 : " + (dao1==dao2));
 		
 		
 
 	}
+	
+	
+	
+	
+	
 
 }
--- spring/Spring-DI/src/member/MemberRegisterService2.java
+++ spring/Spring-DI/src/member/MemberRegisterService2.java
@@ -2,59 +2,38 @@ package member;
 
 import java.util.Date;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service("registService")
-public class MemberRegisterService {
-	
-	@Autowired(required = false)
-	// @Qualifier("sys")
-	//@Resource(name = "memberDao1")
+@Service("registerMemerService")
+public class MemberRegisterService2 {
+
+	@Autowired
 	private MemberDao memberDao;
 	
 	/*
 	 * public void setMemberDao(MemberDao memberDao) { this.memberDao = memberDao; }
 	 */
-	
+
 	/*
-	 * public MemberRegisterService(MemberDao memberDao) { this.memberDao =
+	 * public MemberRegisterService2(MemberDao memberDao) { this.memberDao =
 	 * memberDao; }
 	 */
-	
+
+
 	public void regist(RegisterRequest request) throws AleadyExistingMemberException {
-		
+
 		Member member = memberDao.selectByEmail(request.getEmail());
-		
-		if(member != null) {
+
+		if (member != null) {
 			throw new AleadyExistingMemberException();
 		}
-		
-		Member newMember = new Member(
-				request.getEmail(), 
-				request.getPassword(), 
-				request.getName(), 
-				new Date());
-		
+
+		Member newMember = new Member(request.getEmail(), request.getPassword(), request.getName(), new Date());
+
 		memberDao.insert(newMember);
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
