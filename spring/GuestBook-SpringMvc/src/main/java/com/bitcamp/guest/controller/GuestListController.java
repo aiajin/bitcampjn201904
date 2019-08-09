6822496e57277dc55d116e36f5aa3672e2d773cd
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-08-09 10:27:01 +0900

    member Manager login 2019.08.09

--- spring/GuestBook-SpringMvc/src/main/java/com/bitcamp/guest/controller/GuestListController.java
+++ spring/GuestBook-SpringMvc/src/main/java/com/bitcamp/guest/controller/GuestListController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bitcamp.guest.domain.MessageListView;
 import com.bitcamp.guest.service.GetMessageListService;
@@ -29,6 +30,23 @@ public class GuestListController {
 		model.addAttribute("viewData", listView);
 		
 		return "guest/list";
+	}
+	
+	@RequestMapping("/guest/listJson")
+	@ResponseBody
+	public MessageListView getListJson(
+			@RequestParam(
+					value = "page", 
+					defaultValue = "1") int pageNumber,
+			Model model
+			) {
+		
+		MessageListView listView = 
+				listService.getMessageListView(pageNumber);
+		
+		model.addAttribute("viewData", listView);
+		
+		return listView;
 	}
 
 	
