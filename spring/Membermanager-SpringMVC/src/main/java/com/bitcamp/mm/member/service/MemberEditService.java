fc3481aa662608e465ee63350e47dc06054f0692
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-08-13 10:58:15 +0900

    자동 메퍼 dao 생성

--- spring/Membermanager-SpringMVC/src/main/java/com/bitcamp/mm/member/service/MemberEditService.java
+++ spring/Membermanager-SpringMVC/src/main/java/com/bitcamp/mm/member/service/MemberEditService.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bitcamp.mm.jdbc.ConnectionProvider;
 import com.bitcamp.mm.member.dao.MemberDao;
+import com.bitcamp.mm.member.dao.MemberDaoInterface;
 import com.bitcamp.mm.member.dao.MemberJdbcTemplateDao;
 import com.bitcamp.mm.member.domain.MemberInfo;
 import com.bitcamp.mm.member.domain.RequestMemberEdit;
@@ -22,16 +25,35 @@ public class MemberEditService implements MemberService {
 	// @Autowired
 	// private MemberDao dao;
 
-	@Autowired
-	private MemberJdbcTemplateDao dao;
+	//@Autowired
+	//private MemberJdbcTemplateDao dao;
+	
+	// 자동 메퍼를 이용해서 생성할 dao 
+	private MemberDaoInterface dao;
+	
+	// 자동 메퍼를 위한 sqlSessionTemplate 객체 주입
+	// @Inject : 타입에 맞는 주입 ( java 에서 지원 : 특정 프레임워크에 의존하지 않음 )
+	@Inject
+	private SqlSessionTemplate template;
+	
+	
+	
 
 	public MemberInfo getEditFormData(int id) {
+		
+		// SqlSessionTemplate getMapper 를 이용해 dao 생성
+		dao = template.getMapper(MemberDaoInterface.class);
+		
 		MemberInfo memberInfo = dao.selectMemberByIdx(id);
 		return memberInfo;
 	}
 
 	public int edit(RequestMemberEdit edit, String oldFileName, HttpServletRequest request) {
 
+		
+		// SqlSessionTemplate getMapper 를 이용해 dao 생성
+		dao = template.getMapper(MemberDaoInterface.class);
+		
 		int rCnt = 0;
 		MemberInfo memberInfo = edit.toMemberInfo();
 
