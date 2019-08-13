c3c6bf84622922200f0bc06970711aaeec93da4f
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-08-13 10:49:37 +0900

    자동 메퍼 생성 dao

--- spring/Membermanager-SpringMVC/src/main/java/com/bitcamp/mm/member/service/MemberRegService.java
+++ spring/Membermanager-SpringMVC/src/main/java/com/bitcamp/mm/member/service/MemberRegService.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bitcamp.mm.jdbc.ConnectionProvider;
 import com.bitcamp.mm.member.dao.MemberDao;
+import com.bitcamp.mm.member.dao.MemberDaoInterface;
 import com.bitcamp.mm.member.dao.MemberJdbcTemplateDao;
 import com.bitcamp.mm.member.domain.MemberInfo;
 import com.bitcamp.mm.member.domain.RequestMemberRegist;
@@ -22,12 +25,26 @@ public class MemberRegService implements MemberService {
 	//@Autowired
 	//private MemberDao dao;
 	
-	@Autowired
-	private MemberJdbcTemplateDao dao;
+	//@Autowired
+	//private MemberJdbcTemplateDao dao;
+	
+	// 자동 메퍼를 이용해서 생성할 dao 
+	private MemberDaoInterface dao;
+	
+	// 자동 메퍼를 위한 sqlSessionTemplate 객체 주입
+	// @Inject : 타입에 맞는 주입 ( java 에서 지원 : 특정 프레임워크에 의존하지 않음 )
+	@Inject
+	private SqlSessionTemplate template;
+	
+	
+	
 	
 	public int memberInsert(
 			HttpServletRequest request, 
 			RequestMemberRegist regist) {
+		
+		// SqlSessionTemplate getMapper 를 이용해 dao 생성
+		dao = template.getMapper(MemberDaoInterface.class);
 		
 		// 서버 경로
 		String path = "/uploadfile/userphoto"; // 리소스 매핑 필요
@@ -71,12 +88,18 @@ public class MemberRegService implements MemberService {
 	
 	public char idCheck(String id) {
 		
+		// SqlSessionTemplate getMapper 를 이용해 dao 생성
+		dao = template.getMapper(MemberDaoInterface.class);
+		
 		char chk = dao.selectMemberById(id)==null?'Y':'N' ;
 		
 		return chk;
 	}
 	
 	public String idCheck1(String id) {
+		
+		// SqlSessionTemplate getMapper 를 이용해 dao 생성
+		dao = template.getMapper(MemberDaoInterface.class);
 		
 		String chk = dao.selectMemberById2(id)==null ? "Y" : "N" ;
 		
