bb02ef840824a9c56e47a39ec480e8e4ed046285
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-08-20 15:28:38 +0900

    JavaMailSender 15:28

--- spring/Spring-MVC/src/main/java/com/bitcamp/mvc/MailSendController.java
+++ spring/Spring-MVC/src/main/java/com/bitcamp/mvc/MailSendController.java
@@ -1,5 +1,12 @@
 package com.bitcamp.mvc;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
@@ -14,6 +21,9 @@ public class MailSendController {
 	@Autowired
 	MailSender sender;  
 	
+	@Autowired
+	JavaMailSender javaMailSender;
+	
 	
 	@ResponseBody
 	@RequestMapping("/mail/send")
@@ -21,15 +31,52 @@ public class MailSendController {
 		
 		// 메일 내용 설정
 		SimpleMailMessage message = new SimpleMailMessage();
+		
 		message.setTo("ryuyj76@naver.com");
 		message.setSubject("처음 보내는 Java Mail 입니다.");
 		message.setText("처음 보내는 메일의 내용입니다. 반갑습니다.");
 		message.setFrom("ryuyj@nate.com");
 		
+		SimpleMailMessage m = new SimpleMailMessage(message);
+		
 		sender.send(message);
 		
 		
 		return "send Ok";
+	}
+	
+	@RequestMapping("/mail/send2")
+	public String sendJavaMailSender() {
+		
+		MimeMessage message = javaMailSender.createMimeMessage();
+		
+		try {
+			// 메일 제목 설정
+			message.setSubject("[안내] 처음 보내는 JavaMailSender", "UTF-8");
+			
+			// html 메일 내용
+			String htmlStr = "<h1 style=\"color:blue\">안녕하세요</h1><br>"
+					+ "<a href=\"http://www.naver.com\">naver</a>";
+			// 내용 설정
+			message.setText(htmlStr, "UTF-8", "html");
+			// To 설정
+			message.addRecipient(
+					RecipientType.TO, 
+					new InternetAddress("ryuyj76@naver.com", "유영진 님", "UTF-8"));
+			
+			javaMailSender.send(message);
+			
+			
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return "send ok";
 	}
 
 	
