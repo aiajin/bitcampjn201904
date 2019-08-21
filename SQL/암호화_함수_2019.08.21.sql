c76d2b9753db78554df810b295d9d5a257c2d9f2
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-08-21 13:10:41 +0900

    Create 암호화_함수_2019.08.21.sql

---
+++ SQL/암호화_함수_2019.08.21.sql
@@ -0,0 +1,27 @@
+SELECT * FROM project.member;
+commit;
+
+
+-- 암호화 함수
+select 
+	password(upw) as password, 
+    sha(upw) as sha,
+    sha1(upw) as sha1, 
+    sha2(upw, 256) as sha2, 
+    md5(upw) as md5,
+    HEX(AES_ENCRYPT(upw, 'password')) as AES_ENCRYPT1 ,
+    AES_ENCRYPT(upw, SHA2('password', 256)) as AES_ENCRYPT2
+     
+from project.member limit 0,1;
+
+-- 암호화
+update project.member set upw=HEX(AES_ENCRYPT('1111', 'password')) where idx = 29;
+-- 복호화
+select CONVERT(AES_DECRYPT(UNHEX(upw), 'password') using utf8) from project.member where idx=29;
+select AES_DECRYPT(UNHEX(upw), 'password') from project.member where idx=29;
+
+-- 암호화
+update project.member set upw=HEX(AES_ENCRYPT('1111', SHA2('password', 256))) where idx = 29;
+-- 복호화
+select CONVERT(AES_DECRYPT(UNHEX(upw),  SHA2('password', 256)) using utf8) from project.member where idx=29;
+select AES_DECRYPT(UNHEX(upw),  SHA2('password', 256)) from project.member where idx=29;
