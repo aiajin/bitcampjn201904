4f0ce54de6d37084134358b3d59caf484cab988b
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-08-26 12:53:53 +0900

    jQuery find() 이용한 xml 파싱 12:50

--- spring/Membermanager-SpringMVC/src/main/java/com/bitcamp/mm/member/controller/MemberListController.java
+++ spring/Membermanager-SpringMVC/src/main/java/com/bitcamp/mm/member/controller/MemberListController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -218,6 +219,7 @@ public class MemberListController {
 
 
 
+	@CrossOrigin
 	@RequestMapping("/member/xml/memberList.xml")
 	@ResponseBody
 	public ResponseEntity<ListViewDataXml> memberListXml(
