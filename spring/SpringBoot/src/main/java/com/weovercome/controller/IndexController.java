1e744c1ea4172b39a5f3a03e1aaa0cbaea8ba9ea
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-09-18 13:29:21 +0900

    2019.09.18 13:30

---
+++ spring/SpringBoot/src/main/java/com/weovercome/controller/IndexController.java
@@ -0,0 +1,69 @@
+package com.weovercome.controller;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.weovercome.domain.MemberInfo;
+import com.weovercome.mapper.MemberMapper;
+
+@Controller
+public class IndexController {
+	
+	@Autowired
+	private SqlSessionTemplate template;
+	
+	private MemberMapper mapper;
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		
+		return "Spring Boot Start";
+	}
+	
+	
+	@RequestMapping("/hello")
+	public void hello1() {
+		
+	}
+	
+	@RequestMapping("/memberInfo")
+	public void selectByIdx() {
+		
+		mapper = template.getMapper(MemberMapper.class);
+		
+		MemberInfo info =  mapper.selectMemberById("ryuyj76@naver.com");
+		
+		System.out.println(info);
+				
+	}
+	
+	@RequestMapping("/members")
+	@ResponseBody
+	public List<MemberInfo> selectMembers() {
+		
+		mapper = template.getMapper(MemberMapper.class);
+		
+		List<MemberInfo> info =  mapper.selectAll();
+		
+		for (MemberInfo memberInfo : info) {
+			System.out.println(memberInfo);
+		}
+		
+		return info;
+				
+	}
+
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ spring/SpringBoot/src/main/java/com/weovercome/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.weovercome.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.weovercome.domain.MemberInfo;
+
+public interface MemberMapper {
+	
+	@Select("select * from member order by idx desc")
+	public List<MemberInfo> selectAll();
+	public MemberInfo selectMemberById(String uid);
+
+}
