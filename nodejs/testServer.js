eb7cb87526da949a2c8edc6c8c2d0c28504031cb
유영진 <32862537+ryuyj@users.noreply.github.com>
2019-09-24 13:21:49 +0900

    node -2 기본 모듈

--- nodejs/testServer.js
+++ nodejs/testServer.js
@@ -4,4 +4,4 @@ var http = require('http');  // 웹 서버 생성 모듈
 http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type':'text/html'});
     response.end('<h1> Test Server index Page </h1>');
-}).listen(3000);
+}).listen(3003);
